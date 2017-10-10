f1924e61065516b779a0047cdd9b51b23d2fcd97
jakobzhao <njuzb328>
2017-10-10 13:38:54 -0700

    0.0.1

---
+++ workshop01/readme.md
@@ -0,0 +1,4 @@
+## Tools
+
+- SpatiaLite GUI [https://www.gaia-gis.it/spatialite-2.3.1/binaries.html](https://www.gaia-gis.it/spatialite-2.3.1/binaries.html)
+- QGIS [http://www.qgis.org/en/site/forusers/download.html](http://www.qgis.org/en/site/forusers/download.html)
--- workshop01/twstreams/twstream.py
+++ workshop01/twstreams/twstream.py
@@ -17,19 +17,16 @@ import sqlite3, os, sys
 import time
 from settings import consumer_key, consumer_secret, access_token, access_token_secret
 
-#looking for proxies: http://proxy-list.org/english/index.php
 
+# looking for proxies: http://proxy-list.org/english/index.php
 
-# Go to http://dev.twitter.com and create an app.
-# The consumer key and secret will be generated for you after
-
-
-current_path = os.path.split( os.path.realpath( sys.argv[0] ) )[0]
+current_path = os.path.split(os.path.realpath(sys.argv[0]))[0]
 database = current_path + '/data/tweets.sqlite'
 refresh = False
 
+
 def createDB(database, refresh):
-    current_path = os.path.split( os.path.realpath( sys.argv[0] ) )[0]
+    current_path = os.path.split(os.path.realpath(sys.argv[0]))[0]
     if os.path.exists(database):
         if refresh:
             copy(current_path + '/' + 'twitter_template.sqlite', database)
@@ -41,12 +38,13 @@ class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+
     def on_data(self, data):
         obj = json.loads(data)
         try:
             content = self.processTweet(obj)
             print (content)
-            if content !='':
+            if content != '':
                 cursor.execute(content)
                 conn.commit()
         except:
@@ -82,26 +80,30 @@ class StdOutListener(StreamListener):
             if obj['coordinates']['type'] == "Point":
                 lng = str(obj['coordinates']['coordinates'][0])
                 lat = str(obj['coordinates']['coordinates'][1])
-            #country = obj['place']['country_code']
-            content = 'INSERT INTO tweets (id, created_at, text, source, lang, user_id, user_name, user_screenname, user_description, retweet_count, favorite_count, followers_count, friends_count, statuses_count, user_location, user_verified, placename, placetype, lng, lat) VALUES (%s, "%s", "%s", "%s", "%s", %s, "%s", "%s", "%s", %s, %s, %s, %s, %s, "%s", "%s", "%s", "%s", %s, %s)'% (tid, created_at, text, source, lang, user_id, user_name, user_screenname, user_description, retweet_count, favorite_count, followers_count, friends_count, statuses_count, user_location, user_verified, placename, placetype, lng, lat)
+            # country = obj['place']['country_code']
+            content = 'INSERT INTO tweets (id, created_at, text, source, lang, user_id, user_name, user_screenname, user_description, retweet_count, favorite_count, followers_count, friends_count, statuses_count, user_location, user_verified, placename, placetype, lng, lat) VALUES (%s, "%s", "%s", "%s", "%s", %s, "%s", "%s", "%s", %s, %s, %s, %s, %s, "%s", "%s", "%s", "%s", %s, %s)' % (
+            tid, created_at, text, source, lang, user_id, user_name, user_screenname, user_description, retweet_count,
+            favorite_count, followers_count, friends_count, statuses_count, user_location, user_verified, placename,
+            placetype, lng, lat)
         except:
             content = ''
         return content
 
+
 if __name__ == '__main__':
-	while True:
-		try:
-			createDB(database, refresh)
-			conn = sqlite3.connect(database)
-			cursor = conn.cursor()
-			l = StdOutListener(database)
-			auth = OAuthHandler(consumer_key, consumer_secret)
-			auth.set_access_token(access_token, access_token_secret)
+    while True:
+        try:
+            createDB(database, refresh)
+            conn = sqlite3.connect(database)
+            cursor = conn.cursor()
+            l = StdOutListener(database)
+            auth = OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
             stream = Stream(auth, l)
-            stream.filter()
-            # stream.filter(track=['china'])
+            # stream.filter(track=['china'], async=True)
+            stream.filter(track=['refugee'])
             # stream.filter(track=['beijing hotel', 'shanghai hotel','guangzhou hotel', 'beijing flight', 'shanghai flight', 'guangzhou flight', 'China tourism', 'China travel', 'beijing tourism', 'beijing travel', 'Shanghai tourism', 'shanghai travel','guangdong tourism', 'guangdong travel','great wall tourism', 'great walll travel'])
-            #  stream.filter( locations=[-84,39.5,-82.5,40.2], track=['gay', 'lesbian', 'transgender', 'bisexual', 'queer', 'LGBT', 'lesbo', 'les', 'faggot','fag', 'homo', 'femme', 'hasibian', 'bareback', 'boi', 'chickenhawk', 'trans', 'tranny', 'transvestite', 'trannie', 'shemale', 'drag queen', 'dragqueen'])
+            # stream.filter( locations=[-84,39.5,-82.5,40.2], track=['gay', 'lesbian', 'transgender', 'bisexual', 'queer', 'LGBT', 'lesbo', 'les', 'faggot','fag', 'homo', 'femme', 'hasibian', 'bareback', 'boi', 'chickenhawk', 'trans', 'tranny', 'transvestite', 'trannie', 'shemale', 'drag queen', 'dragqueen'])
             conn.close()
         except:
             time.sleep(2)
